ecaa11e34be83b9be555fb112f187413f56233f8
BodyaKuts <mkkbodya@gmail.com>
2024-07-28 18:29:53 +0300

    test of autodeploy work

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.scss";
+import './App.scss';
 
 export const App = () => {
   return (
     <div className="App">
-      <h1 className="App__title">Template test!</h1>
+      <h1 className="App__title">Template test with autodeploy!</h1>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import "./App.scss";
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import './App.scss';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
+
 root.render(<App />);
