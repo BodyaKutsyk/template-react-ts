cdd0318829f0384bd30bd7843323281d1561be73
BodyaKuts <mkkbodya@gmail.com>
2024-07-28 18:46:37 +0300

    updated files

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.scss';
 export const App = () => {
   return (
     <div className="App">
-      <h1 className="App__title">Template test №2.0 with autodeploy!</h1>
+      <h1 className="App__title">Template test 3.0 with autodeploy!</h1>
+      <h1 className="App__subtitle">!?</h1>
     </div>
   );
 };
