17917f4777153ba523ddc1b2aae55247b3348a6e
Bodya Kutsyk <mkkbodya@gmail.com>
2024-10-16 22:14:44 +0300

    added bulma

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
+import 'bulma/css/bulma.min.css';
 import './App.scss';
 
 export const App = () => {
-  return (
-    <div className="App">
-      <h1 className="App__title">Template test 3.0 with autodeploy!</h1>
-      <h1 className="App__subtitle">!?</h1>
-    </div>
-  );
+  return <></>;
 };
